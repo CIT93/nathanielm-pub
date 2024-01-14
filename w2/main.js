092a07f2f8ce2488d2a8d87e0c603dba768d74e4
Nathaniel2416 <guwop2416@yahoo.com>
2024-01-14 14:24:31 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,17 @@
+const myHousehold = 8
+const homeSize = 7
+const myFood = 8
+const waterConsumption = 1
+const myPurchases = 8
+const myGarbage = 40
+const recycleAmount = 16
+const myTransportation = 4
+
+// Total Carbon Footprint
+
+const carbonFootprint = myHousehold + homeSize + myFood + waterConsumption + myPurchases + myGarbage + recycleAmount + myTransportation
+
+// Output to webpage
+
+const myHeading = document.querySelector("h2");
+myHeading.textContent = carbonFootprint;
