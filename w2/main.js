04b75ff0df9afb9cab4354fcf6a166f517ff5001
Nathaniel2416 <guwop2416@yahoo.com>
2024-01-14 21:17:40 -0800

    coding is done - added semicolons to variables

--- w2/main.js
+++ w2/main.js
@@ -1,15 +1,15 @@
-const myHousehold = 8
-const homeSize = 7
-const myFood = 8
-const waterConsumption = 1
-const myPurchases = 8
-const myGarbage = 40
-const recycleAmount = 16
-const myTransportation = 4
+const myHousehold = 8;
+const homeSize = 7;
+const myFood = 8;
+const waterConsumption = 1;
+const myPurchases = 8;
+const myGarbage = 40;
+const recycleAmount = 16;
+const myTransportation = 4;
 
 // Total Carbon Footprint
 
-const carbonFootprint = myHousehold + homeSize + myFood + waterConsumption + myPurchases + myGarbage + recycleAmount + myTransportation
+const carbonFootprint = myHousehold + homeSize + myFood + waterConsumption + myPurchases + myGarbage + recycleAmount + myTransportation;
 
 // Output to webpage
 
