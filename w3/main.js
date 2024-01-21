cb95d647c870bd1ec3fad9ff586a52c1161baa39
Nathaniel2416 <guwop2416@yahoo.com>
2024-01-21 00:41:25 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,20 +1,29 @@
+function determineHouseholdPts(numberInHousehold) {
+  console.log("Inside the function");
+  if (numberInHousehold === 1) {
+    carbonFootprintPoints = carbonFootprintPoints + 14;
+  } else if (numberInHousehold === 2) {
+    carbonFootprintPoints = carbonFootprintPoints + 12;
+  } else if (numberInHousehold === 3) {
+    carbonFootprintPoints = carbonFootprintPoints + 10;
+  } else if (numberInHousehold === 4) {
+    carbonFootprintPoints = carbonFootprintPoints + 8;
+  } else if (numberInHousehold === 5) {
+    carbonFootprintPoints = carbonFootprintPoints + 6;
+  } else if (numberInHousehold === 6) {
+    carbonFootprintPoints = carbonFootprintPoints + 4;
+  } else if (numberInHousehold >= 7) {
+    carbonFootprintPoints = carbonFootprintPoints + 2;
+  }
+  console.log(
+    `Based on the number of members in the household of ${numberInHousehold} the points would be ${carbonFootprintPoints}`
+  );
+}
 
 let carbonFootprintPoints = 0;
-const numberInHousehold = 3;
+// const numberInHousehold = 12;
 
-if (numberInHousehold === 1) {
-  carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if (numberInHousehold === 2) {
-  carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if (numberInHousehold === 3) {
-  carbonFootprintPoints = carbonFootprintPoints + 10;
-} else if (numberInHousehold === 4) {
-  carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if (numberInHousehold === 5) {
-  carbonFootprintPoints = carbonFootprintPoints + 6;
-} else if (numberInHousehold === 6) {
-  carbonFootprintPoints = carbonFootprintPoints + 4;
-} else if (numberInHousehold >= 7) {
-  carbonFootprintPoints = carbonFootprintPoints + 2;
-}
-console.log(`Based on the number of members in the household of ${numberInHousehold} the points would be ${carbonFootprintPoints}`);
+// global scope
+
+determineHouseholdPts(3);
+determineHouseholdPts(4);
