76aa2be6da1d248e5e21d7662710234a9ae0354c
Nathaniel2416 <guwop2416@yahoo.com>
2024-01-22 12:39:28 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -1,5 +1,5 @@
 function determineHouseholdPts(numberInHousehold) {
-  console.log("Inside the function");
+  let carbonFootprintPoints = 0;
   if (numberInHousehold === 1) {
     carbonFootprintPoints = carbonFootprintPoints + 14;
   } else if (numberInHousehold === 2) {
@@ -16,14 +16,95 @@ function determineHouseholdPts(numberInHousehold) {
     carbonFootprintPoints = carbonFootprintPoints + 2;
   }
   console.log(
-    `Based on the number of members in the household of ${numberInHousehold} the points would be ${carbonFootprintPoints}`
+    `Based on the number of members in a household of ${numberInHousehold} the points would be ${carbonFootprintPoints}.`
   );
+  myHeading.textContent = `Based on the number of members in a household of ${numberInHousehold}, the points would be ${carbonFootprintPoints}.`;
+  return carbonFootprintPoints;
 }
 
+// // const numberInHousehold = 12;
+
+// // global scope
+
+// determineHouseholdPts(3);
+// determineHouseholdPts(4);
+
+// If you have a large house, then add 10 points to your score.
+// If you have a medium-sized house, then add 7 points.
+// If you have a small house, then add 4 points.
+// If you live in an apartment, then add 2 points.
+
+// write a function for considering the size of your home
+// write the code first, then tryto put in a function
+// instead of using a numeric value use a string value
+// example: a = largeHouse
+
+
+//Function to determine house size
+
+function determineHouseSize(houseSize) {
+  let carbonFootprintPoints = 0;
+  switch (houseSize) {                  // Switch statement inside of function
+    case "Large House":
+    case "large house":
+      carbonFootprintPoints = 10;
+      console.log(
+        `Based on the size of your home, your points would be ${carbonFootprintPoints}.`
+      );
+      myHeading2.textContent = `Based on the size of your home, your points would be ${carbonFootprintPoints}.`;
+      break;
+
+    case "Medium House":
+    case "medium house":
+      carbonFootprintPoints = 7;
+      console.log(
+        `Based on the size of your home, your points would be ${carbonFootprintPoints}.`
+      );
+      myHeading2.textContent = `Based on the size of your home, your points would be ${carbonFootprintPoints}.`;
+      break;
+
+    case "Small House":
+    case "small house":
+      carbonFootprintPoints = 4;
+      console.log(
+        `Based on the size of your home, your points would be ${carbonFootprintPoints}.`
+      );
+      myHeading2.textContent = `Based on the size of your home, your points would be ${carbonFootprintPoints}.`;
+      break;
+
+    case "Apartment":
+    case "apartment":
+      carbonFootprintPoints = 2;
+      console.log(
+        `Based on the size of your home, your points would be ${carbonFootprintPoints}.`
+      );
+      myHeading2.textContent = `Based on the size of your home, your points would be ${carbonFootprintPoints}.`;
+      break;
+
+    default: 
+      myHeading2.textContent =
+        "You must choose either a large house, medium house, small house, or apartment.";
+      myHeading3.textContent = `Your total carbon footprint points are ${carbonFootprintPoints}.`;
+      console.log(
+        `Your total carbon footprint points are ${carbonFootprintPoints}.`
+      );
+  }
+  return carbonFootprintPoints; // Return the value of carbonfootprintpoints *must be returned outside of switch statement*
+}
+const myHeading = document.querySelector("h1"); // assigning myHeading to h1
+const myHeading2 = document.querySelector("h2"); // assigning myHeading2 to h2
+const myHeading3 = document.querySelector("h3"); // assigning myHeading3 to h3
+
 let carbonFootprintPoints = 0;
-// const numberInHousehold = 12;
 
-// global scope
+// Calling functions and giving them a variable name to add them together
 
-determineHouseholdPts(3);
-determineHouseholdPts(4);
+let householdPtsResult = determineHouseholdPts(9); // Calling Number of household members function
+
+let houseSizeResult = determineHouseSize("small house"); // Calling House size function
+
+let result = houseSizeResult + householdPtsResult; // Adding the points returned after calling each function and storing the total value of points in a variable named "result"
+
+// Outputting the result of the total points to the webpage and the console
+myHeading3.textContent = `Your total carbon footprint points are ${result}.`;
+console.log(`Your total carbon footprint points are ${result}.`);
